58d5116f3e8f62d99f2d646f33e25abea0a47541
rzqmhb <roziqmahbubi22@gmail.com>
2023-09-12 16:14:32 +0700

    koreksi README & pelengkapan tugas

---
+++ week_3/scr/switchCase.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  int bilangan = 1;
+  switch (bilangan) {
+    case 1: //print(bilangan); print('satu');
+    case 2:
+      //print('dua');
+    case 3:
+      print('tiga');
+    case 4:
+      print('empat');
+      break;
+    default:
+      break;
+  }
+}
