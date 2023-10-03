1b870fdeafaca176e635a4384fe61ebd4229efcd
rzqmhb <roziqmahbubi22@gmail.com>
2023-10-03 16:34:33 +0700

    update laporan

--- week_5/README.md
+++ week_5/README.md
@@ -8,19 +8,40 @@ NIM             : 2141720086
 
 1. Membuat Project, membuat project flutter pada command prompt dapat dilakukan dengan perintah : 'flutter create nama_project'
 2. Mencoba Menjalankan Project
+Virtual Machine :
 ![image](docs/run.png)
 ![image](docs/tampilan_awal.png)
+Device :
+![image](docs/increment_device.jpg)
 3. Membuat Text Widget
+Virtual Machine :
 ![image](docs/test_widget.png)
+Device :
+![image](docs/image_text_device.jpg)
 4. Membuat Image Widget
+Virtual Machine :
 ![image](docs/image_widget.png)
+Device :
+![image](docs/image_text_device.jpg)
 5. Membuat Dialog Widget
+Virtual Machine :
 ![image](docs/dialog_widget.png)
+Device :
+![image](docs/dialog_device.jpg)
 6. Membuat Text Input Widget
+Virtual Machine :
 ![image](docs/input_widget.png)
+Device :
+![image](docs/input_device.jpg)
 7. Membuat Date Selection Widget
+Virtual Machine :
 ![image](docs/selection_widget.png)
+Device :
+![image](docs/date_device1.jpg)
+![image](docs/date_device2.jpg)
 
 ### Hasil Tugas Praktikum :
-
+Virtual Machine :
 ![video](docs/tugas.gif)
+Device :
+![image](docs/tugas_device_emulate.gif)
--- week_5/src/hello_world/lib/main.dart
+++ week_5/src/hello_world/lib/main.dart
@@ -7,37 +7,37 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Contoh Date Picker',
-      home: MyHomePage(title: 'Contoh Date Picker'),
-    );
-  }
-}
-
 // class MyApp extends StatelessWidget {
 //   const MyApp({Key? key}) : super(key: key);
 
 //   @override
 //   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         appBar: AppBar(title: const Text("Contoh TextField")),
-//         body: const TextField(
-//           obscureText: false,
-//           decoration: InputDecoration(
-//             border: OutlineInputBorder(),
-//             labelText: 'Nama',
-//           ),
-//         ),
-//       ),
+//     return const MaterialApp(
+//       title: 'Contoh Date Picker',
+//       home: MyHomePage(title: 'Contoh Date Picker'),
 //     );
 //   }
 // }
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Contoh TextField")),
+        body: const TextField(
+          obscureText: false,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'Nama',
+          ),
+        ),
+      ),
+    );
+  }
+}
 
 // class MyApp extends StatelessWidget {
 //   const MyApp({Key? key}) : super(key: key);
