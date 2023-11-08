2913dcf9c66c1e0db2a8a5ee106673f938c711d3
rzqmhb <roziqmahbubi22@gmail.com>
2023-11-08 07:38:19 +0700

    week 11 : praktikum 2

--- week_11/src/master_plan/lib/main.dart
+++ week_11/src/master_plan/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:master_plan/models/plan.dart';
+import 'package:master_plan/provider/plan_provider.dart';
 import './views/plan_screen.dart';
 
 void main() => runApp(MasterPlanApp());
@@ -9,9 +11,12 @@ class MasterPlanApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
       return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.purple),
-      home: PlanScreen(),
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(primarySwatch: Colors.purple),
+        home: PlanProvider(
+          notifier: ValueNotifier<Plan>(const Plan()),
+          child: const PlanScreen(),
+        ),
       );
     }
   }
--- week_11/src/master_plan/lib/models/plan.dart
+++ week_11/src/master_plan/lib/models/plan.dart
@@ -5,4 +5,13 @@ class Plan {
   final List<Task> tasks;
   
   const Plan({this.name = '', this.tasks = const []});
+
+  int get length => tasks.length;
+
+  int get completedCount => tasks
+    .where((task) => task.complete)
+    .length;
+
+  String get completenessMessage =>
+    '$completedCount out of ${tasks.length} tasks';
 }
