bb1e0b0f0dc8acbac50281cea977e4e0b1658950
rzqmhb <roziqmahbubi22@gmail.com>
2023-11-13 13:19:40 +0700

    praktikum 3 : praktikum

--- week_11/src/master_plan/lib/main.dart
+++ week_11/src/master_plan/lib/main.dart
@@ -1,22 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:master_plan/models/plan.dart';
 import 'package:master_plan/provider/plan_provider.dart';
+import 'package:master_plan/views/plan_creator_screen.dart';
 import './views/plan_screen.dart';
 
-void main() => runApp(MasterPlanApp());
+void main() => runApp(const MasterPlanApp());
 
 class MasterPlanApp extends StatelessWidget {
   const MasterPlanApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-      return MaterialApp(
+    return PlanProvider(
+      notifier: ValueNotifier<List<Plan>>(const []),
+      child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        theme: ThemeData(primarySwatch: Colors.purple),
-        home: PlanProvider(
-          notifier: ValueNotifier<Plan>(const Plan()),
-          child: const PlanScreen(),
+        title: 'State management app Roziq Mahbubi',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
         ),
-      );
-    }
+        home: const PlanCreatorScreen(),
+      ) 
+    );
+    // return MaterialApp(
+    //   debugShowCheckedModeBanner: false,
+    //   theme: ThemeData(primarySwatch: Colors.purple),
+    //   home: PlanProvider(
+    //     notifier: ValueNotifier<Plan>(const Plan()),
+    //     child: const PlanScreen(),
+    //   ),
+    // );
   }
+}
--- week_11/src/master_plan/lib/views/plan_screen.dart
+++ week_11/src/master_plan/lib/views/plan_screen.dart
@@ -4,7 +4,11 @@ import '../models/data_layer.dart';
 import 'package:flutter/material.dart';
 
 class PlanScreen extends StatefulWidget {
-  const PlanScreen({super.key});
+  final Plan plan;
+  const PlanScreen({
+    super.key,
+    required this.plan,
+  });
 
   @override
   State createState() => _PlanScreenState();
@@ -13,6 +17,7 @@ class PlanScreen extends StatefulWidget {
 class _PlanScreenState extends State<PlanScreen> {
   // Plan plan = const Plan();
   late ScrollController scrollController;
+  Plan get plan => widget.plan;
 
   @override
   void initState() {
@@ -24,34 +29,57 @@ class _PlanScreenState extends State<PlanScreen> {
   }
 
   Widget _buildAddTaskButton(BuildContext context) {
-    ValueNotifier<Plan> planNotifier = PlanProvider.of(context);
+    ValueNotifier<List<Plan>> planNotifier = PlanProvider.of(context);
     return FloatingActionButton(
       child: const Icon(Icons.add),
       onPressed: () {
-        Plan currentPlan = planNotifier.value;
-        planNotifier.value = Plan(
+        int planIndex = planNotifier.value.indexWhere((p) => p.name == plan.name);
+        Plan currentPlan = planNotifier.value[planIndex];
+        List<Task> updatedTasks = List<Task>.from(currentPlan.tasks)..add(const Task());
+        planNotifier.value = List<Plan>.from(planNotifier.value)..[planIndex] = Plan(
           name: currentPlan.name,
-          tasks: List<Task>.from(currentPlan.tasks)..add(const Task()),
+          tasks: updatedTasks,
         );
-        // setState(() {
-        //   plan = Plan(
-        //     name: plan.name,
-        //     tasks: List<Task>.from(plan.tasks)
-        //     ..add(const Task()),
-        //   );
-        // });
+        // plan = Plan(
+        //   name: currentPlan.name,
+        //   tasks: updatedTasks,
+        // );
       },
     );
   }
 
+  // Widget _buildAddTaskButton(BuildContext context) {
+  //   ValueNotifier<Plan> planNotifier = PlanProvider.of(context);
+  //   return FloatingActionButton(
+  //     child: const Icon(Icons.add),
+  //     onPressed: () {
+  //       Plan currentPlan = planNotifier.value;
+  //       planNotifier.value = Plan(
+  //         name: currentPlan.name,
+  //         tasks: List<Task>.from(currentPlan.tasks)..add(const Task()),
+  //       );
+  //       // setState(() {
+  //       //   plan = Plan(
+  //       //     name: plan.name,
+  //       //     tasks: List<Task>.from(plan.tasks)
+  //       //     ..add(const Task()),
+  //       //   );
+  //       // });
+  //     },
+  //   );
+  // }
+
   Widget _buildTaskTile(Task task, int index, BuildContext context) {
-    ValueNotifier<Plan> planNotifier = PlanProvider.of(context);
+    ValueNotifier<List<Plan>> planNotifier = PlanProvider.of(context);
+
     return ListTile(
       leading: Checkbox(
-          value: task.complete,
-          onChanged: (selected) {
-            Plan currentPlan = planNotifier.value;
-            planNotifier.value = Plan(
+        value: task.complete,
+        onChanged: (selected) {
+          int planIndex = planNotifier.value.indexWhere((p) => p.name == plan.name);
+          Plan currentPlan = planNotifier.value[planIndex];
+          planNotifier.value = List<Plan>.from(planNotifier.value)
+            ..[planIndex] = Plan(
               name: currentPlan.name,
               tasks: List<Task>.from(currentPlan.tasks)
                 ..[index] = Task(
@@ -59,44 +87,80 @@ class _PlanScreenState extends State<PlanScreen> {
                   complete: selected ?? false,
                 ),
             );
-            // setState(() {
-            //   plan = Plan(
-            //     name: plan.name,
-            //     tasks: List<Task>.from(plan.tasks)
-            //       ..[index] = Task(
-            //         description: task.description,
-            //         complete: selected ?? false,
-            //       ),
-            //   );
-            // });
-          }
-        ),
+        }
+      ),
       title: TextFormField(
         initialValue: task.description,
         onChanged: (text) {
-          Plan currentPlan = planNotifier.value;
-          planNotifier.value = Plan(
-            name: currentPlan.name,
-            tasks: List<Task>.from(currentPlan.tasks)
-              ..[index] = Task(
-                description: text,
-                complete: task.complete,
-              ),
-          );
-          // setState(() {
-          //   plan = Plan(
-          //     name: plan.name,
-          //     tasks: List<Task>.from(plan.tasks)
-          //       ..[index] = Task(
-          //         description: text,
-          //         complete: task.complete,
-          //       ),
-          //   );
-          // });
+          int planIndex = planNotifier.value.indexWhere((p) => p.name == plan.name);
+          Plan currentPlan = planNotifier.value[planIndex];
+          planNotifier.value = List<Plan>.from(planNotifier.value)
+            ..[planIndex] = Plan(
+              name: currentPlan.name, 
+              tasks: List<Task>.from(currentPlan.tasks)
+                ..[index] = Task(
+                  description: text,
+                  complete: task.complete,
+                ),
+            );
         },
       ),
     );
   }
+
+  // Widget _buildTaskTile(Task task, int index, BuildContext context) {
+  //   ValueNotifier<Plan> planNotifier = PlanProvider.of(context);
+  //   return ListTile(
+  //     leading: Checkbox(
+  //         value: task.complete,
+  //         onChanged: (selected) {
+  //           Plan currentPlan = planNotifier.value;
+  //           planNotifier.value = Plan(
+  //             name: currentPlan.name,
+  //             tasks: List<Task>.from(currentPlan.tasks)
+  //               ..[index] = Task(
+  //                 description: task.description,
+  //                 complete: selected ?? false,
+  //               ),
+  //           );
+  //           // setState(() {
+  //           //   plan = Plan(
+  //           //     name: plan.name,
+  //           //     tasks: List<Task>.from(plan.tasks)
+  //           //       ..[index] = Task(
+  //           //         description: task.description,
+  //           //         complete: selected ?? false,
+  //           //       ),
+  //           //   );
+  //           // });
+  //         }
+  //       ),
+  //     title: TextFormField(
+  //       initialValue: task.description,
+  //       onChanged: (text) {
+  //         Plan currentPlan = planNotifier.value;
+  //         planNotifier.value = Plan(
+  //           name: currentPlan.name,
+  //           tasks: List<Task>.from(currentPlan.tasks)
+  //             ..[index] = Task(
+  //               description: text,
+  //               complete: task.complete,
+  //             ),
+  //         );
+  //         // setState(() {
+  //         //   plan = Plan(
+  //         //     name: plan.name,
+  //         //     tasks: List<Task>.from(plan.tasks)
+  //         //       ..[index] = Task(
+  //         //         description: text,
+  //         //         complete: task.complete,
+  //         //       ),
+  //         //   );
+  //         // });
+  //       },
+  //     ),
+  //   );
+  // }
 
   Widget _buildList(Plan plan) {
     return ListView.builder(
@@ -118,42 +182,59 @@ class _PlanScreenState extends State<PlanScreen> {
 
   @override
   Widget build(BuildContext context) {
+    ValueNotifier<List<Plan>> plansNotifier = PlanProvider.of(context);
+
     return Scaffold(
-      appBar: AppBar(title: const Text('Master Plan Roziq Mahbubi')),
-      body: ValueListenableBuilder<Plan>(
-        valueListenable: PlanProvider.of(context),
-        builder: (context, plan, child) {
+      appBar: AppBar(title: Text(plan.name)),
+      body: ValueListenableBuilder<List<Plan>>(
+        valueListenable: plansNotifier,
+        builder: (context, plans, child) {
+          Plan currentPlan = plans.firstWhere((p) => p.name == plan.name);
           return Column(
             children: [
-              Expanded(child: _buildList(plan)),
-              SafeArea(
-                child: Container(
-                  margin: EdgeInsets.only(bottom: 1),
-                  child: Column(
-                    children: [
-                      Text(plan.completenessMessage),
-                      ProgressBar(
-                        width: double.infinity,
-                        value: (plan.completedCount/plan.length),
-                        height: 5.0,
-                        backgroundColor: Colors.grey,
-                        //specify only one: color or gradient
-                        //color:Colors.red,
-                        gradient: LinearGradient(
-                          begin: Alignment.topLeft,
-                          end: Alignment.bottomRight,
-                          colors: [Color.fromARGB(255, 170, 229, 115), Color.fromARGB(255, 108, 223, 0)],
-                        ),
-                      ),
-                    ],
-                  ),
-                )
-              ),
+              Expanded(child: _buildList(currentPlan)),
+              SafeArea(child: Text(currentPlan.completenessMessage)),
             ],
           );
         },
       ),
-      floatingActionButton: _buildAddTaskButton(context),
+      floatingActionButton: _buildAddTaskButton(context,),
     );
-    }
+    // return Scaffold(
+    //   appBar: AppBar(title: const Text('Master Plan Roziq Mahbubi')),
+    //   body: ValueListenableBuilder<Plan>(
+    //     valueListenable: PlanProvider.of(context),
+    //     builder: (context, plan, child) {
+    //       return Column(
+    //         children: [
+    //           Expanded(child: _buildList(plan)),
+    //           SafeArea(
+    //             child: Container(
+    //               margin: EdgeInsets.only(bottom: 1),
+    //               child: Column(
+    //                 children: [
+    //                   Text(plan.completenessMessage),
+    //                   ProgressBar(
+    //                     width: double.infinity,
+    //                     value: (plan.completedCount/plan.length),
+    //                     height: 5.0,
+    //                     backgroundColor: Colors.grey,
+    //                     gradient: LinearGradient(
+    //                       begin: Alignment.topLeft,
+    //                       end: Alignment.bottomRight,
+    //                       colors: [Color.fromARGB(255, 170, 229, 115), Color.fromARGB(255, 108, 223, 0)],
+    //                     ),
+    //                   ),
+    //                 ],
+    //               ),
+    //             )
+    //           ),
+    //         ],
+    //       );
+    //     },
+    //   ),
+    //   floatingActionButton: _buildAddTaskButton(context),
+    // );
   }
+
+}
