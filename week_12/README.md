21c6d774ba86393a7d5d63d41d383aa0cae7755d
rzqmhb <roziqmahbubi22@gmail.com>
2023-11-14 14:38:10 +0700

    week 12 : soal 1

---
+++ week_12/README.md
@@ -0,0 +1,12 @@
+# Pemrograman Mobile - Pertemuan 12
+```
+Nama        : Roziq Mahbubi
+NIM         : 2141720086
+```
+
+## Praktikum
+
+#### Soal 1
+Tambahkan nama panggilan Anda pada title app sebagai identitas hasil pekerjaan Anda.
+##### Jawaban :
+![Alt text](image.png)
---
+++ week_12/src/books/lib/main.dart
@@ -0,0 +1,60 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import "package:http/http.dart";
+import "package:http/http.dart" as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Future Demo Roziq Mahbubi ',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const FuturePage(),
+    );
+  }
+}
+
+class FuturePage extends StatefulWidget {
+  const FuturePage({super.key});
+
+  @override
+  State<FuturePage> createState() => _FuturePageState();
+}
+
+class _FuturePageState extends State<FuturePage> {
+  String result = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Back from the Future'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const Spacer(),
+            ElevatedButton(
+              child: const Text('GO!'),
+              onPressed: () {},
+            ),
+            const Spacer(),
+            Text(result),
+            const Spacer(),
+            const CircularProgressIndicator(),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
